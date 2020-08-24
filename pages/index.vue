13716041edf041e5eb97551a53ef725378689416
Ruben DESERT <ruben.desert@gmail.com>
2020-08-24 02:23:38 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Coucou</p>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+
+@Component
+export default class IndexPage extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
